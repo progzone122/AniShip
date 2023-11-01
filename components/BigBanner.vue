69bc08293f8fda12615e6a55fb52d3a873be3647
diablo <progzone122@gmail.com>
2023-11-02 00:11:33 +0200

    .

--- components/BigBanner.vue
+++ components/BigBanner.vue
@@ -1,18 +1,20 @@
 <template>
   <div v-if="Object.keys(props.anime).length !== 0">
     <div class="w-[100vw] h-[80vh] bg-cover"
-         :style="'background-image: url(https://shikimori.me/' + props.anime.screenshots[0].original ?? null + ')'">
+      :style="'background-image: url(https://shikimori.me/' + props.anime.screenshots[0].original ?? null + ')'">
       <!-- Background Overlay -->
       <div class="w-full h-full inset-0 bg-black opacity-60"></div>
     </div>
     <div class="w-[100vw] relative h-[80vh] top-[-80vh] pl-28 pr-16 pb-16 flex flex-col justify-end">
       <div class="pb-8 h-[70%] flex flex-col justify-end overflow-hidden">
-        <h1 class="font-bold text-5xl pb-6 bigBannerAnimeH1">{{ props.anime.info.russian ?? props.anime.info.name ?? "error" }}</h1>
+        <h1 class="font-bold text-5xl pb-6 bigBannerAnimeH1">{{ props.anime.info.russian ?? props.anime.info.name ??
+          "error" }}</h1>
         <p class="overflow-hidden bigBannerAnimeP">
           {{ stripHtmlTags(props.anime.info.description_html ?? null) }}
         </p>
       </div>
-      <button class="btn btn-accent bigBannerWatchButton w-[12em]" @click="navigateTo({ path: '/watch', query: { id: props.anime.info.id } })">Смотреть</button>
+      <button class="btn btn-accent bigBannerWatchButton w-[12em]"
+        @click="navigateTo({ path: '/watch', query: { id: props.anime.info.id } })">Смотреть</button>
     </div>
   </div>
   <div class="w-[100vw] h-[80vh] bg-black flex justify-center items-center" v-else>
@@ -27,8 +29,7 @@ const props = defineProps({
     default: {},
   },
 });
-//@click="navigateTo({ path: '/watch', query: { id: props.anime.id } })"
-function stripHtmlTags(str) {
+const stripHtmlTags = (str) => {
   // Регулярное выражение для удаления всех HTML-тегов из строки
   return str.replace(/<[^>]+>/g, '');
 }
--- pages/watch.vue
+++ pages/watch.vue
@@ -1,41 +1,52 @@
 <template>
-<div class="w-[100vw] ml-[-5em]">
-  <div class="pl-[6em] pr-[1em] flex gap-4">
-    <!-- Левый блок с постером, названием и кнопками -->
-    <div class="bgColor1 p-4 w-[14em] md:w-[18em] lg:w-[24em] h-fit rounded-xl">
-      <div></div>
-      <div v-if="anime">
-        <div class="w-[100%] h-[14em] md:h-[18em] lg:h-[24em] bg-cover animeCard cursor-pointer" :style="'background-image: url(https://shikimori.me/' + anime.image.original + ')'">
-          <!-- Background Overlay -->
-          <!--      <div class="w-full h-full inset-0 bg-black opacity-60 hidden bg-overlay">-->
-          <!--      </div>-->
-          <!--      <div class="hidden bg-overlay-content relative top-[-12em]">-->
-          <!--      </div>-->
+  <div class="w-[100vw] ml-[-5em]">
+    <div class="pl-[6em] pr-[1em] flex gap-4">
+      <!-- Левый блок с постером, названием и кнопками -->
+      <div class="bgColor1 p-4 w-[14em] md:w-[18em] lg:w-[24em] h-fit rounded-xl">
+        <div></div>
+        <div v-if="anime">
+          <div class="w-[100%] h-[14em] md:h-[18em] lg:h-[24em] bg-cover animeCard cursor-pointer"
+            :style="'background-image: url(https://shikimori.me/' + anime.image.original + ')'">
+            <!-- Background Overlay -->
+            <!--      <div class="w-full h-full inset-0 bg-black opacity-60 hidden bg-overlay">-->
+            <!--      </div>-->
+            <!--      <div class="hidden bg-overlay-content relative top-[-12em]">-->
+            <!--      </div>--> <!-- Временно закоменченно, в будещем можно в оверлей что-то добавить -->
+          </div>
+          <div class="mt-4">
+            <p class="textP2 font-bold">{{ anime.russian ?? anime.name ?? "error" }}</p>
+          </div>
+          <div class="divider"></div>
+          <div class="w-full flex flex-col gap-2">
+            <button class="btn watchButton">
+              Смотреть
+            </button>
+          </div>
         </div>
-        <div class="mt-4">
-          <p class="textP2 font-bold">{{ anime.russian ?? anime.name ?? "error" }}</p>
-        </div>
-        <div class="divider"></div>
-        <div class="w-full flex flex-col gap-2">
-          <button class="btn watchButton">
-            Смотреть
-          </button>
+        <div class="h-[21em] flex justify-center items-center" v-else>
+          <span class="loading loading-ring loading-lg" style="width: 8em;"></span> <!-- loading -->
         </div>
       </div>
-      <div class="h-[21em] flex justify-center items-center" v-else>
-        <span class="loading loading-ring loading-lg" style="width: 8em;"></span> <!-- loading -->
-      </div>
-    </div>
 
-    <!-- Карусель со скриншотами из аниме -->
-    <div class="w-[100%] h-[50vh] p-4 bgColor1 rounded-lg">
-      <WatchAnimeScreenshotsCarousel v-if="anime" :screenshots="anime.screenshots" />
-      <div class="w-full h-[100%] flex justify-center items-center" v-else>
-        <span class="loading loading-ring loading-lg" style="width: 8em;"></span> <!-- loading -->
+      <div class="w-full flex flex-col gap-4">
+        <!-- Карусель со скриншотами из аниме -->
+        <div class="w-full h-[70vh] p-4 bgColor1 rounded-lg">
+          <WatchAnimeScreenshotsCarousel v-if="anime" :screenshots="anime.screenshots" />
+          <div class="w-full h-[100%] flex justify-center items-center" v-else>
+            <span class="loading loading-ring loading-lg" style="width: 8em;"></span> <!-- loading -->
+          </div>
+        </div>
+
+        <!-- Блок с информацией об аниме -->
+        <div class="w-full p-4 bgColor1 rounded-lg">
+          <WatchAnimeInfoAccordion :description="anime.description_html" :genres="anime.genres" v-if="anime" />
+          <div class="h-[35vh] flex justify-center items-center" v-else>
+            <span class="loading loading-ring loading-lg" style="width: 8em;"></span> <!-- loading -->
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 <script setup>
 import Wrapper from "../assets/shikimori/index.js";
